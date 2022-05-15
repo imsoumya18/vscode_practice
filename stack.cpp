ed7cafce2be63409fb0fb164c46eced967a0bdc2
imsoumya18 <soumyadeep184@gmail.com>
2022-05-15 22:30:01 +0530

    stack

---
+++ stack.cpp
@@ -0,0 +1,41 @@
+// @author Soumya
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void rev_string(string s)
+{
+    stack<string> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        string word = "";
+        while (s[i] != ' ' && i < s.length())
+        {
+            word += s[i];
+            i++;
+        }
+        st.push(word);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    string s;
+    getline(cin, s);
+
+    rev_string(s);
+
+    return 0;
+}
