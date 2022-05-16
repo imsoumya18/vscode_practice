b8cd615e2f25b9bdbddfd67fe137e66f00328fe0
imsoumya18 <soumyadeep184@gmail.com>
2022-05-17 00:09:52 +0530

    rev_stack

--- stack.cpp
+++ stack.cpp
@@ -3,27 +3,31 @@
 #include <stack>
 using namespace std;
 
-void rev_stack(stack<int> &st)
+void insert_bottom(stack<int> &st, int top_ele)
 {
     stack<int> temp;
-    int tp;
-
-    while (!st.size() !=0)
+    while (!st.empty())
     {
-        tp = st.top();
+        temp.push(st.top());
         st.pop();
-        while (!st.empty())
-        {
-            temp.push(st.top());
-            st.pop();
-        }
-        while (!temp.empty())
-        {
-            st.push(temp.top());
-            temp.pop();
-        }
-        st.push(tp);
     }
+    st.push(top_ele);
+    while (!temp.empty())
+    {
+        st.push(temp.top());
+        temp.pop();
+    }
+}
+
+void rev_stack(stack<int> &st)
+{
+    if (st.size() == 1)
+        return;
+
+    int top_ele = st.top();
+    st.pop();
+    rev_stack(st);
+    insert_bottom(st, top_ele);
 }
 
 int main()
