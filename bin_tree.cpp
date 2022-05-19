9c19d0760e8a74b064d84e70f01e721e10f4240d
imsoumya18 <soumyadeep184@gmail.com>
2022-05-19 08:27:14 +0530

    bin_tree

---
+++ bin_tree.cpp
@@ -0,0 +1,46 @@
+// @author Soumya
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    /*
+      1
+     / \
+    2   3
+    */
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    /*
+        1
+       / \
+      2   3
+     / \
+    4   5
+    */
+
+    return 0;
+}
