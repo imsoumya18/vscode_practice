146cf19aa9623c78fa24c87f837b5f37a8135cd2
imsoumya18 <soumyadeep184@gmail.com>
2022-05-25 08:18:50 +0530

    bin

--- bin_tree.cpp
+++ bin_tree.cpp
@@ -1,6 +1,5 @@
 // @author Soumya
 #include <iostream>
-#include <queue>
 using namespace std;
 
 struct Node
@@ -17,34 +16,6 @@ struct Node
     }
 };
 
-// height of a binary tree (no of nodes in the longest path)
-int calcHeight(Node *root)
-{
-    if (root == NULL)
-        return 0;
-
-    int lHeight = calcHeight(root->left);
-    int rHeight = calcHeight(root->right);
-
-    return max(lHeight, rHeight) + 1;
-}
-
-// diameter of a binary tree (no of nodes in the longest path between any 2 leaves)
-int calcDiameter(Node *root)
-{
-    if (root == NULL)
-        return 0;
-
-    int lHeight = calcHeight(root->left);
-    int rHeight = calcHeight(root->right);
-    int currDiameter = lHeight + rHeight + 1;
-
-    int lDiameter = calcDiameter(root->left);
-    int rDiameter = calcDiameter(root->right);
-
-    return max(currDiameter, max(lDiameter, rDiameter));
-}
-
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -55,19 +26,21 @@ int main()
     struct Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
     /*
-         1
-       /   \
-      2     3
-     / \   / \
-    4   5 6   7
+      1
+     / \
+    2   3
     */
 
-    cout << calcDiameter(root) << endl;
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    /*
+        1
+       / \
+      2   3
+     / \
+    4   5
+    */
 
     return 0;
 }
