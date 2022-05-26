301a2cf460e5a3abd239320507c30195976f7524
imsoumya18 <soumyadeep184@gmail.com>
2022-05-26 07:47:11 +0530

    vdiknvidn

--- leetcode2.cpp
+++ leetcode2.cpp
@@ -1,29 +1,85 @@
 // @author Soumya
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-double myPow(double x, int n)
+struct TreeNode
 {
-    if (n == 0)
-        return 1;
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
 
-    long long nn = n;
-
-    if (nn < 0)
+    TreeNode(int data)
     {
-        x = 1 / x;
-        nn = nn * (-1);
+        val = data;
+        left = NULL;
+        right = NULL;
     }
+};
 
-    if (nn % 2 == 0)
+int search(int curr, vector<int> &inorder, int start, int end)
+{
+    int i;
+    for (i = start; i <= end; i++)
+        if (curr == inorder[i])
+            return i;
+
+    return -1;
+}
+
+TreeNode *proper(vector<int> &preorder, vector<int> &inorder, int start, int end)
+{
+    int idx = 0;
+
+    if (start > end)
+        return NULL;
+
+    int curr = preorder[idx];
+    idx++;
+    TreeNode *node = new TreeNode(curr);
+
+    if (start == end)
+        return node;
+
+    int pos = search(curr, inorder, start, end);
+    node->left = proper(preorder, inorder, start, pos - 1);
+    node->right = proper(preorder, inorder, pos + 1, end);
+
+    return node;
+}
+
+TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+{
+    return proper(preorder, inorder, 0, inorder.size() - 1);
+}
+
+void printLevelOrder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+
+    queue<TreeNode *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
     {
-        double temp = myPow(x, nn / 2);
-        return temp * temp;
-    }
-    else
-    {
-        double temp = myPow(x, nn - 1);
-        return x * temp;
+        TreeNode *node = q.front();
+        q.pop();
+        if (node != NULL)
+        {
+            cout << node->val << " ";
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        else if (!q.empty())
+        {
+            cout << endl;
+            q.push(NULL);
+        }
     }
 }
 
@@ -34,10 +90,18 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    double x = -2;
-    int n = 5;
+    struct TreeNode *root = new TreeNode(1);
 
-    cout << myPow(x, n);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    vector<int> preorder{3, 9, 20, 15, 7};
+    vector<int> inorder{9, 3, 15, 20, 7};
+    printLevelOrder(buildTree(preorder, inorder));
 
     return 0;
 }
