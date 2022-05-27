4db0b090b70848b2d8eabe7c973bebcb2d647d96
imsoumya18 <soumyadeep184@gmail.com>
2022-05-27 07:54:50 +0530

    search

---
+++ linear_search.cpp
@@ -0,0 +1,39 @@
+// @author Soumya
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// binary search algorithm
+int binary_search(vector<int> vct, int n, int first, int last)
+{
+    if (first <= last)
+    {
+        int mid = first + (last - first) / 2;
+
+        if (vct[mid] == n)
+            return mid;
+
+        else if (vct[mid] > n)
+            return binary_search(vct, n, first, mid - 1);
+
+        else
+            return binary_search(vct, n, mid + 1, last);
+    }
+
+    return -1;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    vector<int> vct{-35, -2, 27, 48, 95};
+    int n = 95;
+
+    cout << binary_search(vct, n, 0, vct.size() - 1);
+
+    return 0;
+}
