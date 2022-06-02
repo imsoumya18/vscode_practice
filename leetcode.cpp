755d7dffd24fdce6316b642ad54c63711fac9e98
imsoumya18 <soumyadeep184@gmail.com>
2022-06-02 17:56:02 +0530

    merge sort

--- leetcode.cpp
+++ leetcode.cpp
@@ -6,24 +6,7 @@ using namespace std;
 
 void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 {
-    int i = 0, j = 0;
-
-    while (i <= m || j <= n)
-    {
-        if (nums1[i] <= nums2[j])
-        {
-            i++;
-        }
-        else
-        {
-            for (int k = m + n - 2; k >= i; k--)
-                nums1[k + 1] = nums1[k];
-
-            nums1[i] = nums2[j];
-            i++;
-            j++;
-        }
-    }
+    vector<>
 }
 
 int main()
