40682d9fe36a71a76ed3546cb5da178adc45e68d
imsoumya18 <soumyadeep184@gmail.com>
2022-06-02 16:56:10 +0530

    insertion

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,57 +1,30 @@
 // @author Soumya
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
-vector<vector<int>> kClosest(vector<vector<int>> &points, int k)
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 {
-    vector<pair<int, pair<int, int>>> v;
-    vector<vector<int>> res;
+    int i = 0, j = 0;
 
-    for (auto i : points)
+    while (i <= m || j <= n)
     {
-        int x = i[0] * i[0] + i[1] * i[1];
-        v.push_back({x, {i[0], i[1]}});
-    }
+        if (nums1[i] <= nums2[j])
+        {
+            i++;
+        }
+        else
+        {
+            for (int k = m + n - 2; k >= i; k--)
+                nums1[k + 1] = nums1[k];
 
-    for (int i = 0; i < v.size() - 1; i++)
-        for (int j = 0; j < v.size() - i - 1; j++)
-            if (v[j + 1].first < v[j].first)
-                swap(v[j], v[j + 1]);
-    
-    for(int i=0; i<k;i++){
-        vector<int> temp;
-        temp.push_back(v[i].second.first);
-        temp.push_back(v[i].second.second);
-        res.push_back(temp);
-        temp.clear();
+            nums1[i] = nums2[j];
+            i++;
+            j++;
+        }
     }
-
-    return res;
 }
-
-// vector<vector<int>> kClosest(vector<vector<int>> &points, int k)
-// {
-//     vector<vector<int>> val;
-//     vector<vector<int>> res;
-
-//     for (int i = 0; i < points.size(); i++)
-//     {
-//         vector<int> temp;
-//         temp.push_back(i);
-//         temp.push_back(points[i][0] * points[i][0] + points[i][1] * points[i][1]);
-//         val.push_back(temp);
-//     }
-
-//     for (int i = 0; i < val.size() - 1; i++)
-//         for (int j = 0; j < val.size() - i - 1; j++)
-//             if (val[j + 1][1] < val[j][1])
-//                 swap(val[j], val[j + 1]);
-
-//     val.erase(1, 2);
-
-//     return val;
-// }
 
 int main()
 {
@@ -60,17 +33,13 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    vector<vector<int>> points{{3, 3}, {5, -1}, {-2, 4}}, clop;
-    int k = 2;
+    vector<int> nums1{1, 2, 3, 0, 0, 0}, nums2{2, 5, 6};
+    int m = 3, n = 3;
 
-    clop = kClosest(points, k);
+    merge(nums1, m, nums2, n);
 
-    for (auto i : clop)
-    {
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;
-    }
+    for (auto i : nums1)
+        cout << i << " ";
 
     return 0;
 }
