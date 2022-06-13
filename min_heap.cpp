da1d5843536d188912596b7392203e014b18ca16
imsoumya18 <soumyadeep184@gmail.com>
2022-06-13 19:59:38 +0530

    test

--- min_heap.cpp
+++ min_heap.cpp
@@ -3,22 +3,63 @@
 #include <vector>
 using namespace std;
 
-// min heap insertion
+// max heap insertion
 void insert(vector<int> &harr, int p)
 {
     harr.push_back(p);
 
-    int n = harr.size() - 1, flag = 0;
+    int n = harr.size() - 1;
 
-    while (flag != 1)
+    while (n > 1)
     {
-        if (harr[(n - 1) / 2] > harr[n])
+        if (harr[(n - 1) / 2] < harr[n])
+        {
             swap(harr[(n - 1) / 2], harr[n]);
 
-        n = (n - 1) / 2;
+            n = (n - 1) / 2;
+        }
+        else
+            break;
+    }
+    if (harr[(n - 1) / 2] < harr[n])
+    {
+        swap(harr[(n - 1) / 2], harr[n]);
 
-        if (n == 0)
-            flag++;
+        n = (n - 1) / 2;
+    }
+}
+
+// min heap deletion
+void deletion(vector<int> &harr)
+{
+    harr[0] = harr[harr.size() - 1];
+    harr.pop_back();
+    int i = 0, n = harr.size();
+
+    while (i < n)
+    {
+        int lc = (i + 1) * 2, rc = (i + 1) * 2 + 1;
+
+        if (harr[lc] > harr[rc])
+        {
+            if (harr[lc] > harr[i])
+            {
+                swap(harr[lc], harr[i]);
+                i = lc;
+            }
+            else
+                break;
+        }
+        else
+        {
+            if (harr[rc] > harr[i])
+            {
+                swap(harr[rc], harr[i]);
+                i = rc;
+            }
+            else
+                break;
+        }
     }
 }
 
@@ -29,9 +70,10 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    vector<int> harr{0, 8, 1, 17, 18, 12, 5};
+    vector<int> harr{35, 30, 27, 20, 15, 12, 17, 10};
 
-    insert(harr, 15);
+    insert(harr, 28);
+    deletion(harr);
 
     for (auto i : harr)
         cout << i << " ";
