051fb54c9709273eed7f9cc0b78fac7ca3a38147
imsoumya18 <soumyadeep184@gmail.com>
2022-07-05 22:44:45 +0530

    tctu

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,21 +5,35 @@
 #include <set>
 using namespace std;
 
-vector<vector<string>> groupAnagrams(vector<string> &strs)
+vector<int> executeInstructions(int n, vector<int> &startPos, string s)
 {
-    vector<vector<string>> vct;
-    set<string> s;
-
-    for (auto p : strs)
-    {
-        for (int i = 0; i < p.size() - 1; i++)
-            for (int j = i + 1; j < p.size(); j++)
-                if (p[j] < p[i])
-                    swap(p[i], p[j]);
-    }
+    vector<int> vct;
 
     for (int i = 0; i < s.size(); i++)
-        cout << s[i] << endl;
+    {
+        int cnt = 0;
+
+        for (int j = i; j < s.size(); j++)
+        {
+            if (s[j] == 'R')
+                startPos[1]++;
+            else if (s[j] == 'L')
+                startPos[1]--;
+            else if (s[j] == 'U')
+                startPos[0]--;
+            else
+                startPos[0]++;
+
+            if (startPos[0] < 0 || startPos[0] >= n || startPos[1] < 0 || startPos[1] >= n)
+                break;
+
+            cnt++;
+        }
+
+        vct.push_back(cnt);
+    }
+
+    return vct;
 }
 
 int main()
@@ -29,16 +43,12 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    vector<string> strs{"eat", "tea", "tan", "ate", "nat", "bat"};
+    string s = "RRDDLU";
+    int n = 3;
+    vector<int> startPos{0, 1};
 
-    groupAnagrams(strs);
-
-    // for (auto i : groupAnagrams(strs))
-    // {
-    //     for (auto j : i)
-    //         cout << j << " ";
-    //     cout << endl;
-    // }
+    for (auto i : executeInstructions(n, startPos, s))
+        cout << i << " ";
 
     return 0;
 }
