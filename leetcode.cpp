7ac8d510e34c95bb5f85690d68bd45a4a6b656b9
imsoumya18 <soumyadeep184@gmail.com>
2022-07-06 17:35:49 +0530

    test

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,37 +5,7 @@
 #include <set>
 using namespace std;
 
-vector<int> executeInstructions(int n, vector<int> &startPos, string s)
-{
-    vector<int> vct;
-
-    for (int i = 0; i < s.size(); i++)
-    {
-        int cnt = 0;
-        vector<int> sp = startPos;
-
-        for (int j = i; j < s.size(); j++)
-        {
-            if (s[j] == 'R')
-                sp[1]++;
-            else if (s[j] == 'L')
-                sp[1]--;
-            else if (s[j] == 'U')
-                sp[0]--;
-            else
-                sp[0]++;
-
-            if (sp[0] < 0 || sp[0] >= n || sp[1] < 0 || sp[1] >= n)
-                break;
-
-            cnt++;
-        }
-
-        vct.push_back(cnt);
-    }
-
-    return vct;
-}
+vector<vector<int>> groupThePeople(vector<int> &groupSizes) {}
 
 int main()
 {
@@ -44,12 +14,7 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    string s = "RRDDLU";
-    int n = 3;
-    vector<int> startPos{0, 1};
-
-    for (auto i : executeInstructions(n, startPos, s))
-        cout << i << " ";
+    vector<int> groupSizes{3, 3, 3, 3, 3, 1, 3};
 
     return 0;
 }
