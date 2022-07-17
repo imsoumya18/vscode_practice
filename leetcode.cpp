ade755114b1738508734d2f8b0f2f511c98753be
imsoumya18 <soumyadeep184@gmail.com>
2022-07-17 20:39:21 +0530

    hiibib

--- leetcode.cpp
+++ leetcode.cpp
@@ -10,7 +10,7 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     vector<int> vct;
     int i = 0, j = 0, tot = nums1.size() + nums2.size();
 
-    while (i < nums1.size() || j < nums2.size())
+    while (i < nums1.size() && j < nums2.size())
     {
         if (nums1[i] < nums2[j])
         {
@@ -22,29 +22,31 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
             vct.push_back(nums2[j]);
             j++;
         }
-        if (i == nums1.size())
-        {
-            vct.push_back(nums2[j]);
-            j++;
-        }
-        if (j == nums2.size())
-        {
-            vct.push_back(nums1[i]);
-            i++;
-        }
+    }
+
+    while (i < nums1.size())
+    {
+        vct.push_back(nums1[i]);
+        i++;
+    }
+    while (j < nums2.size())
+    {
+        vct.push_back(nums2[j]);
+        j++;
     }
 
     for (auto i : vct)
         cout << i << endl;
+    cout << endl;
 
     if (tot % 2 == 0)
     {
-        double ans = (vct[tot / 2] + vct[tot / 2 - 1]) / tot;
+        double ans = (vct[tot / 2] + vct[tot / 2 - 1]) / 2.0;
         return ans;
     }
     else
     {
-        double ans = vct[tot / 2] / tot;
+        double ans = vct[tot / 2.0];
         return double(ans);
     }
 }
