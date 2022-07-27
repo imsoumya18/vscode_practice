505927d9062b7da0bd461e3b24c4dea1eb1d8975
imsoumya18 <soumyadeep184@gmail.com>
2022-07-27 20:42:30 +0530

    kjfvbj

--- leetcode.cpp
+++ leetcode.cpp
@@ -11,7 +11,7 @@ bool if_palindrome(string s)
     for (int i = 0; i < s.size() / 2; i++)
         if (s[i] != s[s.size() - i - 1])
         {
-            flag++;
+            flag = 1;
             break;
         }
 
