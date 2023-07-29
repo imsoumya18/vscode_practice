f05675a852c26b76092ecb4438179ce2ee6242cd
soumya <soumyadeep184@gmail.com>
2023-07-29 23:07:11 +0530

    dewydc

--- leetcode.cpp
+++ leetcode.cpp
@@ -3,37 +3,28 @@
 #include <vector>
 using namespace std;
 
-vector<int> spiralOrder(vector<vector<int>> &matrix)
+int gcd(int a, int b)
 {
-    vector<int> res;
-    int it, rowMin = 0, colMin = 0, rowMax = matrix.size() - 1, colMax = matrix[0].size() - 1;
+    if (b > a)
+        swap(a, b);
 
-    while (rowMin <= rowMax && colMin <= colMax)
+    if (b == 0)
+        return a;
+
+    return gcd(a % b, b);
+}
+
+int findGCD(vector<int> &nums)
+{
+    int maxi = nums[0], mini = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
     {
-        for (it = colMin; it <= colMax; it++)
-            res.push_back(matrix[rowMin][it]);
-        rowMin++;
-
-        for (it = rowMin; it <= rowMax; it++)
-            res.push_back(matrix[it][colMax]);
-        colMax--;
-
-        if (rowMin <= rowMax)
-        {
-            for (it = colMax; it >= colMin; it--)
-                res.push_back(matrix[rowMax][it]);
-            rowMax--;
-        }
-
-        if (colMin <= colMax)
-        {
-            for (it = rowMax; it >= rowMin; it--)
-                res.push_back(matrix[it][colMin]);
-            colMin++;
-        }
+        maxi = max(maxi, nums[i]);
+        mini = min(mini, nums[i]);
     }
 
-    return res;
+    return gcd(maxi, mini);
 }
 
 int main()
@@ -43,19 +34,9 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    vector<vector<int>> nums{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+    vector<int> nums{2, 5, 6, 9, 10};
 
-    for (auto i : nums)
-    {
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;
-    }
-
-    vector<int> ans = spiralOrder(nums);
-
-    for (auto i : ans)
-        cout << i << " ";
+    cout << findGCD(nums) << endl;
 
     return 0;
 }
