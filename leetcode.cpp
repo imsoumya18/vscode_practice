945dce0d854b65989397ce637c8a024b3d6fd641
soumya <soumyadeep184@gmail.com>
2023-07-30 23:17:30 +0530

    fdsf

--- leetcode.cpp
+++ leetcode.cpp
@@ -6,44 +6,24 @@
 #include <algorithm>
 using namespace std;
 
-bool search(vector<vector<int>> matrix, int n, int m, int target)
+vector<vector<int>> transpose(vector<vector<int>> &matrix)
 {
-    int lo = 0, hi = n - 1;
-    int midrow = (lo + hi) / 2;
+    int ro = matrix.size(), col = matrix[0].size();
+    vector<vector<int>> ans;
 
-    while (lo <= hi)
+    for (int i = 0; i < col; i++)
     {
-        if ((midrow == n - 1) || (matrix[midrow][0] <= target && matrix[midrow+1][0] > target))
-            break;
+        vector<int> row;
 
-        else if (matrix[midrow][0] > target)
-            hi = midrow - 1;
+        for (int j = 0; j < ro; j++)
+        {
+            row.push_back(matrix[j][i]);
+        }
 
-        else
-            lo = midrow + 1;
-
-        midrow = (lo + hi) / 2;
+        ans.push_back(row);
     }
 
-    lo = 0;
-    hi = m - 1;
-    int midcol = (lo + hi) / 2;
-
-    while (lo <= hi)
-    {
-        if (matrix[midrow][midcol] == target)
-            return true;
-
-        else if (matrix[midrow][midcol] > target)
-            hi = midcol - 1;
-
-        else
-            lo = midcol + 1;
-
-        midcol = (lo + hi) / 2;
-    }
-
-    return false;
+    return ans;
 }
 
 int main()
@@ -54,20 +34,24 @@ int main()
 #endif
 
     vector<vector<int>> matrix{
-        {3, 4, 5, 13, 13, 15, 15, 24, 30, 31},
-        {13, 14, 19, 19, 27, 31, 32, 33, 35, 62},
-        {14, 23, 24, 25, 36, 37, 38, 44, 44, 63},
-        {30, 34, 36, 38, 40, 42, 43, 47, 55, 65},
-        {36, 39, 40, 41, 42, 51, 51, 58, 59, 69},
-        {43, 44, 44, 49, 56, 59, 64, 64, 75, 82},
-        {46, 46, 47, 63, 64, 66, 67, 70, 85, 89},
-        {54, 57, 58, 65, 67, 70, 74, 88, 88, 93},
-        {70, 72, 75, 76, 90, 90, 91, 93, 93, 98},
-        {84, 85, 93, 95, 96, 97, 97, 99, 100, 100}};
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12}};
 
-    int target = 63;
+    for (auto i : matrix)
+    {
+        for (auto j : i)
+            cout << j << "  ";
+        cout << endl;
+    }
 
-    cout << search(matrix, 10, 10, target) << endl;
+    for (auto i : transpose(matrix))
+    {
+        for (auto j : i)
+            cout << j << "  ";
+        cout << endl;
+    }
 
     return 0;
 }
