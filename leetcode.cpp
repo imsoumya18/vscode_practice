61a65a68414850476cdfe79cef2610eff3bdb9a5
soumya <soumyadeep184@gmail.com>
2023-08-05 20:29:38 +0530

    ghcg

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,25 +1,10 @@
 // @author Soumya
 #include <iostream>
 #include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
 using namespace std;
 
-vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+int minimumSeconds(vector<int> &nums)
 {
-    set<int> s, ans;
-
-    for (auto i : nums1)
-        s.insert(i);
-
-    for (auto j : nums2)
-        if (s.find(j) != s.end())
-            ans.insert(j);
-
-    vector<int> vct(ans.begin(), ans.end());
-
-    return vct;
 }
 
 int main()
@@ -28,13 +13,6 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-
-    vector<int> nums1{1, 2, 2, 1}, nums2{2, 2};
-
-    vector<int> vct = intersection(nums1, nums2);
-
-    for (auto i : vct)
-        cout << i << endl;
 
     return 0;
 }
