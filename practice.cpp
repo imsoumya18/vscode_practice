13d000b17e7a9ba45524708f2132784c32c188e7
soumya <soumyadeep184@gmail.com>
2023-08-16 13:33:18 +0530

    dsa

--- practice.cpp
+++ practice.cpp
@@ -3,127 +3,22 @@
 #include <vector>
 using namespace std;
 
-void selection_sort(vector<int> &vct)
+vector<vector<int>> subsets(vector<int> &nums)
 {
-    int n = vct.size();
+    vector<vector<int>> ans{{}}, temp;
+    temp = ans;
 
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
-            if (vct[j] < vct[i])
-                swap(vct[i], vct[j]);
-}
-
-void bubble_sort(vector<int> &vct)
-{
-    int n = vct.size();
-
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
-            if (vct[j + 1] < vct[j])
-                swap(vct[j], vct[j + 1]);
-}
-
-void insertion_sort(vector<int> &vct)
-{
-    int n = vct.size();
-
-    for (int i = 1; i < n; i++)
+    for (auto i : nums)
     {
-        int temp = vct[i];
-        int j = i - 1;
-
-        while (vct[j] > temp && j >= 0)
+        for (auto j : temp)
         {
-            vct[j + 1] = vct[j];
-            j--;
+            j.push_back(i);
+            ans.push_back(j);
         }
-
-        vct[j + 1] = temp;
-    }
-}
-
-void merge(vector<int> &vct, int l, int mid, int r)
-{
-    vector<int> vct1, vct2;
-
-    for (int i = l; i <= mid; i++)
-        vct1.push_back(vct[i]);
-
-    for (int i = mid + 1; i <= r; i++)
-        vct2.push_back(vct[i]);
-
-    int i = l, j = 0, k = 0;
-
-    while (j < vct1.size() && k < vct2.size())
-    {
-        if (vct1[j] < vct2[k])
-        {
-            vct[i] = vct1[j];
-            i++;
-            j++;
-        }
-        else
-        {
-            vct[i] = vct2[k];
-            i++;
-            k++;
-        }
+        temp = ans;
     }
 
-    while (j < vct1.size())
-    {
-        vct[i] = vct1[j];
-        i++;
-        j++;
-    }
-
-    while (k < vct2.size())
-    {
-        vct[i] = vct2[k];
-        i++;
-        k++;
-    }
-}
-
-void merge_sort(vector<int> &vct, int l, int r)
-{
-    if (l < r)
-    {
-        int mid = (l + r) / 2;
-
-        merge_sort(vct, l, mid);
-        merge_sort(vct, mid + 1, r);
-
-        merge(vct, l, mid, r);
-    }
-}
-
-int partition(vector<int> &vct, int l, int r)
-{
-    int pivot = vct[r];
-    int i = l - 1;
-
-    for (int j = l; j < r; j++)
-        if (vct[j] < pivot)
-        {
-            i++;
-            swap(vct[i], vct[j]);
-        }
-
-    i++;
-    swap(vct[i], vct[r]);
-    return i;
-}
-
-void quick_sort(vector<int> &vct, int l, int r)
-{
-    if (l < r)
-    {
-        int pi = partition(vct, l, r);
-
-        quick_sort(vct, l, pi - 1);
-        quick_sort(vct, pi + 1, r);
-    }
+    return ans;
 }
 
 int main()
@@ -132,17 +27,17 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    vector<int> vct{9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-    for (auto i : vct)
-        cout << i << " ";
-    cout << endl;
+    vector<int> nums{2, 4};
 
-    quick_sort(vct, 0, vct.size() - 1);
+    vector<vector<int>> ans = subsets(nums);
 
-    for (auto i : vct)
-        cout << i << " ";
-    cout << endl;
+    for (auto i : ans)
+    {
+        for (auto j : i)
+            cout << j << " ";
+        cout << endl;
+    }
 
     return 0;
 }
