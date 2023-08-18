218d2c7f2a070611dc51adac6a2ac689b57df4d0
soumya <soumyadeep184@gmail.com>
2023-08-18 14:02:49 +0530

    fsdkjskd

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,70 +5,21 @@
 #include <algorithm>
 using namespace std;
 
-bool isSafe(int x, int y, vector<vector<int>> &m, int n, vector<vector<int>> visited)
+int maxSubStr(string str)
 {
-    if (x >= 0 && x < n && y >= 0 && y < n && visited[x][y] == 0 && m[x][y] == 1)
-        return true;
+    int ans = 0;
+    int curr1 = 0, curr2 = 0;
 
-    return false;
-}
+    for (int i = 0; i < str.size(); i++)
+        if (i > 0 && str[i] == str[0] && str[i - 1] != str[0])
+        {
+            ans++;
 
-void solve(vector<vector<int>> &m, int n, vector<vector<int>> visited, vector<string> &ans, int x, int y, string path)
-{
-    if (x == n - 1 && y == n - 1)
-    {
-        ans.push_back(path);
-        return;
-    }
+            if (curr2 > curr1)
+                curr2 -= curr1;
+        }
 
-    visited[x][y] = 1;
-
-    if (isSafe(x - 1, y, m, n, visited))
-    {
-        path.append("U");
-        solve(m, n, visited, ans, x - 1, y, path);
-        path.pop_back();
-    }
-
-    if (isSafe(x + 1, y, m, n, visited))
-    {
-        path.append("D");
-        solve(m, n, visited, ans, x + 1, y, path);
-        path.pop_back();
-    }
-
-    if (isSafe(x, y - 1, m, n, visited))
-    {
-        path.append("L");
-        solve(m, n, visited, ans, x, y - 1, path);
-        path.pop_back();
-    }
-
-    if (isSafe(x, y + 1, m, n, visited))
-    {
-        path.append("R");
-        solve(m, n, visited, ans, x, y + 1, path);
-        path.pop_back();
-    }
-
-    visited[x][y] = 0;
-}
-
-vector<string> findPath(vector<vector<int>> &m, int n)
-{
-    vector<string> ans;
-
-    if (m[0][0] == 0)
-        return ans;
-
-    vector<vector<int>> visited(n, vector<int>(n, 0));
-    string path = "";
-
-    int x = 0, y = 0;
-    solve(m, n, visited, ans, x, y, path);
-    sort(ans.begin(), ans.end());
-
-    return ans;
+    return -1;
 }
 
 int main()
@@ -78,17 +29,9 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    vector<vector<int>> m{{1, 0, 0, 0},
-                          {1, 1, 0, 1},
-                          {1, 1, 0, 0},
-                          {0, 1, 1, 1}};
+    string str = "0100110101";
 
-    int n = 4;
-
-    vector<string> vct = findPath(m, n);
-
-    for (auto i : vct)
-        cout << i << endl;
+    cout << maxSubStr(str) << endl;
 
     return 0;
 }
