3f948003e9008f0a2297bfbb277fe3824c0803ab
soumya <soumyadeep184@gmail.com>
2023-08-22 14:15:55 +0530

    dsa

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,115 +1,175 @@
-// @author Soumya
-#include <iostream>
-#include <cmath>
-using namespace std;
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 
-// linked list implementation
-class ListNode
-{
-public:
-    int val;
-    ListNode *next;
-
-    ListNode(int x)
-    {
-        val = x;
-        next = nullptr;
-    }
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
 };
 
-// insert node at head
-void insertAtHead(ListNode *&head, int val)
-{
-    ListNode *temp = head;
+using namespace std;
 
-    head = new ListNode(val);
-    head->next = temp;
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
 }
 
-// insert node at end
-void insertAtEnd(ListNode *&head, int val)
-{
-    ListNode *n = new ListNode(val);
+Node* flatten (Node* root);
 
-    if (head == nullptr)
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+
+Node *merge(Node *l1, Node *l2)
+{
+    if (l1 == nullptr)
+        return l2;
+
+    if (l2 == nullptr)
+        return l1;
+
+    Node *ans = new Node(-1), *temp = ans;
+
+    while (l1 != nullptr && l2 != nullptr)
     {
-        head = n;
-        return;
+        if (l1->data < l2->data)
+        {
+            temp->next = l1;
+            temp = l1;
+            l1 = l1->bottom;
+        }
+        else
+        {
+            temp->next = l2;
+            temp = l2;
+            l2 = l2->bottom;
+        }
     }
 
-    ListNode *temp = head;
-
-    while (temp->next != nullptr)
-        temp = temp->next;
-
-    temp->next = n;
-}
-
-// print elements of linked list
-void display(ListNode *&head)
-{
-    ListNode *temp = head;
-
-    while (temp != nullptr)
+    while (l1 != nullptr)
     {
-        cout << temp->val << "-->";
-        temp = temp->next;
+        temp->next = l1;
+        temp = l1;
+        l1 = l1->bottom;
     }
 
-    cout << "NULL" << endl;
-}
-
-long long multiplyTwoLists(ListNode *l1, ListNode *l2)
-{
-    long long n1 = 0, n2 = 0;
-
-    ListNode *temp;
-
-    temp = l1;
-
-    while (temp != nullptr)
+    while (l2 != nullptr)
     {
-        n1 *= 10;
-        n1 += temp->val;
-
-        temp = temp->next;
+        temp->next = l2;
+        temp = l2;
+        l2 = l2->bottom;
     }
 
-    temp = l2;
-
-    while (temp != nullptr)
-    {
-        n2 *= 10;
-        n2 += temp->val;
-
-        temp = temp->next;
-    }
-
-    return (n1 * n2);
+    return ans->next;
 }
-
-int main()
+    
+Node *flatten(Node *root)
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
-    ListNode *head = new ListNode(2);
-    insertAtEnd(head, 4);
-    insertAtEnd(head, 6);
-    insertAtEnd(head, 8);
-
-    display(head);
-
-    ListNode *head2 = new ListNode(4);
-    insertAtEnd(head2, 5);
-
-    display(head2);
-    long long int x = pow(10, 9) + 7;
-
-    cout << typeof(x) << endl;
-    // cout << multiplyTwoLists(head, head2) << endl;
-
-    return 0;
+    if(root->next == nullptr)
+    {
+        // root->next = root->bottom;
+        return root;
+    }
+    
+    Node *right = flatten(root->next);
+    root->next = nullptr;
+    
+    Node *ans = merge(root, right);
+    
+    return ans;
+   // Your code here
+        
 }
+
--- practice.cpp
+++ practice.cpp
@@ -1,22 +1,130 @@
 // @author Soumya
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<vector<int>> subsets(vector<int> &nums)
+// linked list implementation
+class ListNode
 {
-    vector<vector<int>> ans{{}}, temp;
-    temp = ans;
+public:
+    int val;
+    ListNode *next;
+    ListNode *bottom;
 
-    for (auto i : nums)
+    ListNode(int x)
     {
-        for (auto j : temp)
-        {
-            j.push_back(i);
-            ans.push_back(j);
-        }
-        temp = ans;
+        val = x;
+        next = nullptr;
+        bottom = nullptr;
     }
+};
+
+// insert node at head
+void insertAtHead(ListNode *&head, int val)
+{
+    ListNode *temp = head;
+
+    head = new ListNode(val);
+    head->next = temp;
+}
+
+// insert node at end
+void insertAtEnd(ListNode *&head, int val)
+{
+    ListNode *n = new ListNode(val);
+
+    if (head == nullptr)
+    {
+        head = n;
+        return;
+    }
+
+    ListNode *temp = head;
+
+    while (temp->next != nullptr)
+        temp = temp->next;
+
+    temp->next = n;
+}
+
+// print elements of linked list
+void display(ListNode *&head)
+{
+    ListNode *temp = head;
+
+    while (temp != nullptr)
+    {
+        cout << temp->val << "-->";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+void display2(ListNode *&head)
+{
+    ListNode *temp = head;
+
+    while (temp != nullptr)
+    {
+        cout << temp->val << "-->";
+        temp = temp->bottom;
+    }
+
+    cout << "NULL" << endl;
+}
+
+ListNode *merge(ListNode *l1, ListNode *l2)
+{
+    if (l1 == nullptr)
+        return l2;
+
+    if (l2 == nullptr)
+        return l1;
+
+    ListNode *ans = new ListNode(-1), *temp = ans;
+
+    while (l1 != nullptr && l2 != nullptr)
+    {
+        if (l1->val < l2->val)
+        {
+            temp->bottom = l1;
+            temp = l1;
+            l1 = l1->bottom;
+        }
+        else
+        {
+            temp->bottom = l2;
+            temp = l2;
+            l2 = l2->bottom;
+        }
+    }
+
+    while (l1 != nullptr)
+    {
+        temp->bottom = l1;
+        temp = l1;
+        l1 = l1->bottom;
+    }
+
+    while (l2 != nullptr)
+    {
+        temp->bottom = l2;
+        temp = l2;
+        l2 = l2->bottom;
+    }
+
+    return ans->bottom;
+}
+
+ListNode *flatten(ListNode *root)
+{
+    if (root->next == nullptr)
+        return root;
+
+    ListNode *right = flatten(root->next);
+    root->next = nullptr;
+
+    ListNode *ans = merge(root, right);
 
     return ans;
 }
@@ -28,16 +136,38 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    vector<int> nums{2, 4};
+    ListNode *head = new ListNode(5);
+    insertAtEnd(head, 10);
+    insertAtEnd(head, 19);
+    insertAtEnd(head, 28);
 
-    vector<vector<int>> ans = subsets(nums);
+    head->bottom = new ListNode(7);
+    head->bottom->bottom = new ListNode(8);
+    head->bottom->bottom->bottom = new ListNode(30);
 
-    for (auto i : ans)
-    {
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;
-    }
+    head->next->bottom = new ListNode(20);
+
+    head->next->next->bottom = new ListNode(22);
+    head->next->next->bottom->bottom = new ListNode(50);
+
+    head->next->next->next->bottom = new ListNode(35);
+    head->next->next->next->bottom->bottom = new ListNode(40);
+    head->next->next->next->bottom->bottom->bottom = new ListNode(45);
+
+    display(head);
+    cout << endl
+         << endl;
+
+    display2(head);
+    display2(head->next);
+    display2(head->next->next);
+    display2(head->next->next->next);
+    cout << endl
+         << endl;
+
+    ListNode *ans = flatten(head);
+
+    display2(ans);
 
     return 0;
 }
