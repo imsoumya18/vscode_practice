8513a091406f889dfd63bc027953e88870ef5531
soumya <soumyadeep184@gmail.com>
2023-08-27 08:34:16 +0530

    2 done

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,84 +1,26 @@
 // @author Soumya
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <set>
 using namespace std;
 
-vector<int> prevSmaller(vector<int> &heights)
+long long minimumPossibleSum(int n, int target)
 {
-	stack<int> st;
-	st.push(-1);
+	set<int> st;
 
-	int n = heights.size();
-	vector<int> ans(n, -1);
+	st.insert(1);
 
-	for (int i = 0; i < n; i++)
+	long long ans = 1;
+	int it = 2;
+
+	while (st.size() != n)
 	{
-		while (st.top() != -1 && heights[st.top()] >= heights[i])
-			st.pop();
-		ans[i] = st.top();
-		st.push(i);
-	}
-
-	return ans;
-}
-
-vector<int> nextSmaller(vector<int> &heights)
-{
-	stack<int> st;
-	st.push(-1);
-
-	int n = heights.size();
-	vector<int> ans(n, -1);
-
-	for (int i = n - 1; i >= 0; i--)
-	{
-		while (st.top() != -1 && heights[st.top()] >= heights[i])
-			st.pop();
-		ans[i] = st.top();
-		st.push(i);
-	}
-
-	return ans;
-}
-
-int largestRectangleArea(vector<int> &heights)
-{
-	vector<int> prev = prevSmaller(heights);
-	vector<int> next = nextSmaller(heights);
-
-	int ans = 0;
-	int n = heights.size();
-
-	for (int i = 0; i < n; i++)
-	{
-		int l = heights[i];
-		if (next[i] == -1)
-			next[i] = n;
-		int b = next[i] - prev[i] - 1;
-		int area = l * b;
-
-		if (area > ans)
-			ans = area;
-	}
-
-	return ans;
-}
-
-int maximalRectangle(vector<vector<char>> &matrix)
-{
-	int ans = 0;
-	vector<int> heights(matrix[0].size(), 0);
-
-	for (auto row : matrix)
-	{
-		for (int i = 0; i < matrix[0].size(); i++)
-			if (row[i] == '0')
-				heights[i] = 0;
-			else
-				heights[i]++;
-
-		ans = max(ans, largestRectangleArea(heights));
+		if (st.find(target - it) == st.end())
+		{
+			st.insert(it);
+			ans += it;
+		}
+		it++;
 	}
 
 	return ans;
@@ -91,13 +33,9 @@ int main()
 	freopen("output.txt", "w", stdout);
 #endif
 
-	vector<vector<char>> matrix{
-		{'1', '0', '1', '0', '0'},
-		{'1', '0', '1', '1', '1'},
-		{'1', '1', '1', '1', '1'},
-		{'1', '0', '0', '1', '0'}};
+	int n = 2, target = 3;
 
-	cout << maximalRectangle(matrix) << endl;
+	cout << minimumPossibleSum(n, target) << endl;
 
 	return 0;
 }
