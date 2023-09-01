37a6c9ba0032b57e3af6744c7ee7162e7d4f5fae
soumya <soumyadeep184@gmail.com>
2023-09-01 23:25:32 +0530

    lc

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,59 +1,17 @@
 // @author Soumya
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
-struct TreeNode
+vector<int> countBits(int n)
 {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
+	vector<int> vct(n + 1, 0);
 
-	TreeNode(int data)
-	{
-		val = data;
-		left = nullptr;
-		right = nullptr;
-	}
-};
-
-vector<vector<int>> printLevelOrder(TreeNode *root)
-{
-	vector<vector<int>> vct;
-	vector<int> curr;
-
-	if (root == nullptr)
-		return vct;
-
-	queue<TreeNode *> q;
-	q.push(root);
-	q.push(nullptr);
-
-	while (!q.empty())
-	{
-		TreeNode *node = q.front();
-		q.pop();
-
-		if (node != nullptr)
-		{
-			curr.push_back(node->val);
-
-			if (node->left)
-				q.push(node->left);
-
-			if (node->right)
-				q.push(node->right);
-		}
-		else if (!q.empty())
-		{
-			vct.push_back(curr);
-			curr.clear();
-			q.push(nullptr);
-		}
-	}
-
-	vct.push_back(curr);
+	for (int i = 1; i <= n; i++)
+		if (i % 2 == 0)
+			vct[i] = vct[i / 2];
+		else
+			vct[i] = vct[i / 2] + 1;
 
 	return vct;
 }
@@ -65,17 +23,10 @@ int main()
 	freopen("output.txt", "w", stdout);
 #endif
 
-	struct TreeNode *root = new TreeNode(1);
-	root->left = new TreeNode(2);
-	root->right = new TreeNode(3);
-	root->left->right = new TreeNode(4);
+	int n = 5;
 
-	for (auto i : printLevelOrder(root))
-	{
-		for (auto j : i)
-			cout << j << " ";
-		cout << endl;
-	}
+	for (auto i : countBits(n))
+		cout << i << endl;
 
 	return 0;
 }
