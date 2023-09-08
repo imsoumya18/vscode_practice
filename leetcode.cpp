366d9222a291e4793c4fa3a884810f1a7bfd49d8
soumya <soumyadeep184@gmail.com>
2023-09-08 22:26:50 +0530

    daily

--- leetcode.cpp
+++ leetcode.cpp
@@ -3,102 +3,29 @@
 #include <vector>
 using namespace std;
 
-// linked list implementation
-class ListNode
+vector<vector<int>> generate(int numRows)
 {
-public:
-	int val;
-	ListNode *next;
+	vector<vector<int>> vct;
 
-	ListNode(int x)
+	vct.push_back(vector<int>{1});
+
+	if (numRows == 1)
+		return vct;
+
+	int cnt = 1;
+
+	while (cnt != numRows)
 	{
-		val = x;
-		next = nullptr;
-	}
-};
+		vector<int> last = vct.back();
+		vector<int> newRow{1};
 
-// insert node at head
-void insertAtHead(ListNode *&head, int val)
-{
-	ListNode *temp = head;
+		for (int i = 0; i < last.size() - 1; i++)
+			newRow.push_back(last[i] + last[i + 1]);
 
-	head = new ListNode(val);
-	head->next = temp;
-}
+		newRow.push_back(1);
 
-// insert node at end
-void insertAtEnd(ListNode *&head, int val)
-{
-	ListNode *n = new ListNode(val);
-
-	if (head == nullptr)
-	{
-		head = n;
-		return;
-	}
-
-	ListNode *temp = head;
-
-	while (temp->next != nullptr)
-		temp = temp->next;
-
-	temp->next = n;
-}
-
-// print elements of linked list
-void display(ListNode *&head)
-{
-	ListNode *temp = head;
-
-	while (temp != nullptr)
-	{
-		cout << temp->val << "-->";
-		temp = temp->next;
-	}
-
-	cout << "NULL" << endl;
-}
-
-vector<ListNode *> splitListToParts(ListNode *head, int k)
-{
-	vector<ListNode *> vct(k, nullptr);
-
-	ListNode *it = head;
-	int cnt = 0;
-
-	while (it != nullptr)
-	{
-		it = it->next;
+		vct.push_back(newRow);
 		cnt++;
-	}
-
-	vector<int> cnt_list(k, cnt / k);
-
-	int rem = cnt % k;
-
-	for (int i = 0; i < rem; i++)
-		cnt_list[i]++;
-
-	it = head;
-
-	for (int i = 0; i < k; i++)
-	{
-		if (it == nullptr)
-			break;
-
-		cnt = 0;
-
-		vct[i] = it;
-
-		while (cnt != cnt_list[i] - 1)
-		{
-			it = it->next;
-			cnt++;
-		}
-
-		ListNode *prev = it;
-		it = it->next;
-		prev->next = nullptr;
 	}
 
 	return vct;
@@ -111,23 +38,14 @@ int main()
 	freopen("output.txt", "w", stdout);
 #endif
 
-	ListNode *head = new ListNode(1);
-	insertAtEnd(head, 2);
-	insertAtEnd(head, 3);
-	// insertAtEnd(head, 4);
-	// insertAtEnd(head, 5);
-	// insertAtEnd(head, 6);
-	// insertAtEnd(head, 7);
-	// insertAtEnd(head, 8);
-	// insertAtEnd(head, 9);
-	// insertAtEnd(head, 10);
+	int numRows = 5;
 
-	display(head);
-
-	int k = 5;
-
-	for (auto i : splitListToParts(head, k))
-		display(i);
+	for (auto i : generate(numRows))
+	{
+		for (auto j : i)
+			cout << j << " ";
+		cout << endl;
+	}
 
 	return 0;
 }
