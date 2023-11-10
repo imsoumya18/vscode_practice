2fdb49e64fc1a2d6ce4454bda08c7dd4dbc2506b
imsoumya18 <soumyadeep184@gmail.com>
2023-11-10 23:40:49 +0530

    fgfbg

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,9 +5,22 @@
 #include <string>
 using namespace std;
 
-void test(int x)
+int peakIndexInMountainArray(vector<int> &arr)
 {
-	cout << x << endl;
+	int lo = 0, hi = arr.size() - 1;
+	int mid = lo + (hi - lo) / 2;
+
+	while (lo < hi)
+	{
+		if (mid == arr.size() - 1 || arr[mid] > arr[mid + 1])
+			hi = mid;
+		else if (mid == 0 || arr[mid] > arr[mid - 1])
+			lo = mid + 1;
+
+		mid = lo + (hi - lo) / 2;
+	}
+
+	return mid;
 }
 
 int main()
@@ -17,9 +30,10 @@ int main()
 	freopen("output.txt", "w", stdout);
 #endif
 
-	int x = 40;
+	vector<int> arr{0, 1, 0};
+	int target = 8;
 
-	test(x);
+	cout << peakIndexInMountainArray(arr) << endl;
 
 	return 0;
 }
