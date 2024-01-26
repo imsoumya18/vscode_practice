d0ddf090543e909a7e35246ade59d648c51ddc91
imsoumya18 <soumyadeep184@gmail.com>
2024-01-26 23:20:01 +0530

    je

--- leetcode.cpp
+++ leetcode.cpp
@@ -23,45 +23,6 @@ void print2d(vector<vector<int>> vct)
 	}
 }
 
-int nCr(int n, int r)
-{
-	int res = 1;
-
-	for (int i = 0; i < r; i++)
-	{
-		res *= (n - i);
-		res /= (i + 1);
-	}
-
-	return res;
-}
-
-vector<vector<int>> generate(int numRows)
-{
-	vector<vector<int>> vct{};
-
-	for (int row = 1; row <= numRows; row++)
-	{
-		int res = 1;
-
-		vector<int> temp{};
-
-		temp.push_back(res);
-
-		for (int col = 1; col < row; col++)
-		{
-			res *= (row - col);
-			res /= col;
-
-			temp.push_back(res);
-		}
-
-		vct.push_back(temp);
-	}
-
-	return vct;
-}
-
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -69,11 +30,9 @@ int main()
 	freopen("output.txt", "w", stdout);
 #endif
 
-	int numRows = 5;
+	vector<int> vct{1, 2, 3}, vct2{1, 2, 3};
 
-	vector<vector<int>> vct = generate(numRows);
-
-	print2d(vct);
+	cout << (vct == vct2) << endl;
 
 	return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -3,78 +3,32 @@
 #include <vector>
 using namespace std;
 
-struct TreeNode
+int lin_search(vector<int> vct, int k)
 {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode(int data)
-    {
-        val = data;
-        left = nullptr;
-        right = nullptr;
-    }
-};
-
-void preorder(struct TreeNode *root)
-{
-    if (root == nullptr)
-        return;
-
-    cout << root->val << " ";
-    preorder(root->left);
-    preorder(root->right);
-}
-
-void postorder(struct TreeNode *root)
-{
-    if (root == nullptr)
-        return;
-
-    postorder(root->left);
-    postorder(root->right);
-    cout << root->val << " ";
-}
-
-void inorder(struct TreeNode *root)
-{
-    if (root == nullptr)
-        return;
-
-    inorder(root->left);
-    cout << root->val << " ";
-    inorder(root->right);
-}
-
-int search(int in[], int curr, int start, int end)
-{
-    for (int i = start; i <= end; i++)
-        if (in[i] == curr)
+    for (int i = 0; i < vct.size(); i++)
+        if (vct[i] == k)
             return i;
 
     return -1;
 }
 
-TreeNode *buildTree(int pre[], int in[], int start, int end)
+int bin_search(vector<int> vct, int k, int l, int r)
 {
-    static int idx = 0;
+    int mid = l + (r - l) / 2;
 
-    if (start > end)
-        return nullptr;
+    while (l < r)
+    {
+        if (vct[mid] == k)
+            return mid;
+        else if (vct[mid] > k)
+            r = mid - 1;
+        else
+            l = mid + 1;
 
-    int curr = pre[idx];
-    idx++;
-    TreeNode *node = new TreeNode(curr);
+        mid = l + (r - l) / 2;
+    }
 
-    if (start == end)
-        return node;
-
-    int pos = search(in, curr, start, end);
-    node->left = buildTree(pre, in, start, pos - 1);
-    node->right = buildTree(pre, in, pos + 1, end);
-
-    return node;
+    return -1;
 }
 
 int main()
@@ -84,37 +38,10 @@ int main()
     freopen("output.txt", "w", stdout);
 #endif
 
-    struct TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-    root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(5);
-    root->left->right->left = new TreeNode(6);
-    root->left->right->right = new TreeNode(7);
+    vector<int> vct{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int k = 60;
 
-    // cout << "Preorder:" << endl;
-    // preorder(root);
-    // cout << endl;
-
-    // cout << "Postorder:" << endl;
-    // postorder(root);
-    // cout << endl;
-
-    // cout << "Inorder:" << endl;
-    // inorder(root);
-    // cout << endl;
-
-    int pre[] = {1, 2, 4, 5, 6, 7, 3}, in[] = {4, 2, 6, 5, 7, 1, 3};
-
-    TreeNode *n = buildTree(pre, in, 0, 6);
-
-    cout << "Preorder:" << endl;
-    preorder(n);
-    cout << endl;
-
-    cout << "Inorder:" << endl;
-    inorder(n);
-    cout << endl;
+    cout << bin_search(vct, k, 0, vct.size() - 1) << endl;
 
     return 0;
 }
