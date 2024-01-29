ee60b008ef8ec3a3486e517f23ce49654e2bd281
imsoumya18 <soumyadeep184@gmail.com>
2024-01-29 19:45:55 +0530

    w

--- gfg.cpp
+++ gfg.cpp
@@ -4,26 +4,72 @@
 #include <stack>
 using namespace std;
 
-bool checkStrings(string s1, string s2)
+vector<int> findUnion(int arr1[], int arr2[], int n, int m)
 {
-    for (int i = 0; i < s1.size() - 2; i++)
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr1[j] < arr1[i])
+                swap(arr1[i], arr1[j]);
+
+    for (int i = 0; i < m - 1; i++)
+        for (int j = i + 1; j < m; j++)
+            if (arr2[j] < arr2[i])
+                swap(arr2[i], arr2[j]);
+
+    vector<int> vct;
+    int i = 0, j = 0;
+
+    while (i < n && j < m)
     {
-        int j;
+        if (arr1[i] < arr2[j])
+        {
+            vct.push_back(arr1[i]);
 
-        if (i % 2 == 0)
-            j = 0;
+            int curr = arr1[i];
+            while (curr == arr1[i] && i < n)
+                i++;
+        }
+        else if (arr1[i] > arr2[j])
+        {
+            vct.push_back(arr2[j]);
+
+            int curr = arr2[j];
+            while (curr == arr2[j] && j < m)
+                j++;
+        }
         else
-            j = 1;
+        {
+            vct.push_back(arr1[i]);
 
-        for (j; j < s1.size(); j += 2)
-            if (i != j && s1[i] == s2[j] && s1[j] == s2[i])
-            {
-                swap(s1[i], s1[j]);
-                break;
-            }
+            int curr = arr1[i];
+            while (curr == arr1[i] && i < n)
+                i++;
+
+            curr = arr2[j];
+            while (curr == arr2[j] && j < m)
+                j++;
+        }
     }
 
-    return (s1 == s2);
+    while (i < n)
+    {
+        vct.push_back(arr1[i]);
+
+        int curr = arr1[i];
+        while (curr == arr1[i] && i < n)
+            i++;
+    }
+
+    while (j < m)
+    {
+        vct.push_back(arr2[j]);
+
+        int curr = arr2[j];
+        while (curr == arr2[j] && j < m)
+            j++;
+    }
+
+    return vct;
 }
 
 int main()
@@ -32,9 +78,13 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    string s1 = "abcdba", s2 = "cabdab";
+    int arr1[] = {5, 4, 6, 6, 3, 2, 1, 3, 5, 2}, arr2[] = {3, 2, 1, 2, 2};
+    int n = 8, m = 5;
 
-    cout << checkStrings(s1, s2) << endl;
+    vector<int> res = findUnion(arr1, arr2, n, m);
+
+    for (auto i : res)
+        cout << i << " ";
 
     return 0;
 }
